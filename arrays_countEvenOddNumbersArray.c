7e6c9bc8b38b8e422f3820ae1f373bfec55de44d
Prakhar Chaudhary <prakharchaudhary13@gmail.com>
2025-03-26 20:48:26 +0530

    Create arrays_countEvenOddNumbersArray.c

---
+++ arrays_countEvenOddNumbersArray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int size;
+    scanf("%d",&size);
+    int a[size],even=0,odd=0;
+    for(int i=0;i<size;i++){
+        scanf("%d",&a[i]);
+        if(a[i]%2==0)
+            even++;
+        else
+            odd++;
+    }
+    printf("%d %d",even,odd);
+     return 0;
+}
