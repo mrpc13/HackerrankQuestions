242fa01cefafe74da41478f54da7be73cb73767a
Prakhar Chaudhary <prakharchaudhary13@gmail.com>
2025-03-28 04:05:59 +0530

    Create array_rotateByKPositions.c

---
+++ array_rotateByKPositions.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdio.h>
+
+
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void rotateArray(int arr[], int n, int k) {
+    k = k % n; 
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, n - 1);
+    reverse(arr, 0, n - 1);
+}
+int main() {
+    int n, k;
+    // Reading input
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &k);
+
+    // Call the function to rotate the array
+    rotateArray(arr, n, k);
+    
+    // Print the rotated array
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
