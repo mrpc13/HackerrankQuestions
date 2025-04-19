e187368684c10f98987f630fbe848cbf76769f9f
Prakhar Chaudhary <prakharchaudhary13@gmail.com>
2025-04-19 21:20:46 +0530

    Update bitwise_extractithbit.c

--- bitwise_extractithbit.c
+++ bitwise_extractithbit.c
@@ -3,9 +3,6 @@
 int main() {
     int num,ibit;
     scanf("%d \n%d",&num,&ibit);
-    
-        printf("%d",num=num >> ibit & 1);
-    
-    
+    printf("%d",num=num >> ibit & 1);
     return 0;
 }
